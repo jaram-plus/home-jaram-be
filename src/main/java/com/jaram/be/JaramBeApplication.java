e1aff268bb53fb265cca398156c2023c5a49284f
Sognare <developer.ksb@gmail.com>
2026-06-26 15:17:20 +0900

    init: spring project

---
+++ src/main/java/com/jaram/be/JaramBeApplication.java
@@ -0,0 +1,13 @@
+package com.jaram.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaramBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaramBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaram/be/JaramBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaram.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaramBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
